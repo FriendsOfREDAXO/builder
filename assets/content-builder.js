9f0d706b1b9261164407d92fc556f01f220fce17
Thomas Skerbis <thomas.skerbis@mac.com>
2026-06-26 11:50:42 +0200

    Hotfix: rex:ready für nachfolgende Items entfernt - verursachte Chaos

--- assets/content-builder.js
+++ assets/content-builder.js
@@ -3493,9 +3493,6 @@
                                 $input.attr('name', newName);
                             }
                         });
-                        
-                        // REX:READY Event triggern um Editoren neu zu initialisieren
-                        $(document).trigger('rex:ready', [$item]);
                     });
                 } else {
                     // Fallback: am Ende einfügen
